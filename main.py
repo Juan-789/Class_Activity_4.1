e17f66f16bf72b5d6e85ea1a9b2ee9e5466a9bee
JuanMarulanda4 <79994699+Juan-789@users.noreply.github.com>
2021-11-24 16:49:58 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+"""
+1. Write a program that takes from the user a number and prints Xs vertically.
+"""
+numTimes = int(input("How many times do you want me to iterate X? "))
+for i in range (numTimes):
+  print("X")
+#prints "X" the n amount of times that the user wants to
